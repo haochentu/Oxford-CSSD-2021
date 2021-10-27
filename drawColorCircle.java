806b8601891ef0452c8ea77b1259dc3073861588
haochentu <haochentu@outlook.com>
2021-10-27 14:35:23 +0100

    draw a colored circle without Design Pattern

---
+++ drawColorCircle.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+public class drawColorCircle extends JFrame{
+
+	public drawColorCircle(){
+		setTitle("Draw Circle");
+		setSize(600,600);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+	}
+
+	//make oval into a circle
+	public void paint(Graphics g){
+		Graphics2D gg = (Graphics2D) g;
+		gg.drawOval(200,200,200,200); //drawOval is a Java method in Graphics. 
+		// the parameters pass are drawOval(int x, int y, int width, int height)
+		// by changing the number in parameters we can control the size of the circle
+		// the resource to understand drawOval(): https://chortle.ccsu.edu/java5/notes/chap36/ch36_5.html
+		gg.setPaint(Color.red);  //get the red paintbrush
+		gg.fillOval(200,200,200,200);  //paint red in the dimension I set which is the same coordination as my circle. 
+		// the resource to understand color: https://www.tabnine.com/code/java/methods/java.awt.Graphics/fillOval
+	
+		
+	}
+	
+	public static void main(String[] args) {
+		new drawColorCircle();
+	
+	}
+}
+
+
+//my resource to learn draw circles: https://www.delftstack.com/howto/java/draw-a-circle-in-java/	
