825c1ae7ba91e2418aab8a0e2b7c05306b27859a
Haochen Tu <67667094+haochentu@users.noreply.github.com>
2021-10-27 22:34:45 +0100

    Create singleRCircle.java

---
+++ Circle_Singleton/singleRCircle.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+public class SingleRCircle {
+
+	public static void main(String[] args) {
+		SingleCircle object = SingleCircle.getInstance();
+		
+		object.drawCircle();
+
+	}
+
+}
