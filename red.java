e7062f95e78bd1be3faea289d81f1e51f8f88730
Haochen Tu <67667094+haochentu@users.noreply.github.com>
2021-10-27 18:08:19 +0100

    Update red.java

--- red.java
+++ red.java
@@ -1,11 +1,19 @@
 //concrete class for color red
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 public class red implements color{
 	public void fill(Graphics g,int x,int y,int xx, int yy){
-		x = 200;
-		y = 200;
-		xx = 200;
-		yy = 200;
+		//x = 200;
+		//y = 200;
+		//xx = 200;
+		//yy = 200;
 		Graphics2D gg = (Graphics2D) g;
 		gg.setPaint(Color.red);  //get the red paintbrush
 		gg.fillOval(x,y,xx,yy);  //paint red in the dimension I set which is the same coordination as my circle. 
+	}
 }
