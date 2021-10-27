860db43a77e952cbfd451ab29b0b88858256f38b
haochentu <haochentu@outlook.com>
2021-10-27 17:34:18 +0100

    implementation to draw red circle for bridge pattern
    circle class for bridge pattern

---
+++ drawDCircles.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+public class drawDCircles{
+	public static void main(String[] args{
+		Shape redCircle = new Circle(200,200,200,200, new red());
+		
+		redCircle.draw();
+		
+	}
+}
