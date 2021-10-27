e99e08d3180f3411d9e2433d35a9839dac6dfcad
Haochen Tu <67667094+haochentu@users.noreply.github.com>
2021-10-27 18:07:31 +0100

    Update drawDCircles.java

--- drawDCircles.java
+++ drawDCircles.java
@@ -4,11 +4,17 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
-public class drawDCircles{
-	public static void main(String[] args{
-		Shape redCircle = new Circle(200,200,200,200, new red());
+public class drawDifferentCircles extends JFrame{
+	public drawCircle(){
+		setTitle("Draw Circle");
+		setSize(600,600);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
-		redCircle.draw();
+	public static void main(String[] args{
+		new drawCircle();
+		Shape redCircle = new Circle(200,200,200,200, new red());
+		red.draw();
 		
 	}
 }
