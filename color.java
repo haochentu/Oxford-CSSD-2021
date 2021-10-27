81be79071afcd58e277bbf4a585046e980ab8323
haochentu <haochentu@outlook.com>
2021-10-27 17:27:08 +0100

    color interface for Bridge Pattern

---
+++ color.java
@@ -0,0 +1,12 @@
+//color interface
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+
+public interface color{
+	public void fill(Graphics g, int x, int y, int xx, int yy);
+}
