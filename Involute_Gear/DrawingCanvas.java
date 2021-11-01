161695caac7ed721df3b88b7387473ad4b7f5dbc
Haochen Tu <67667094+haochentu@users.noreply.github.com>
2021-11-01 11:01:06 +0000

    Update DrawingCanvas.java

--- Involute_Gear/DrawingCanvas.java
+++ Involute_Gear/DrawingCanvas.java
@@ -10,6 +10,13 @@ public class DrawingCanvas extends JComponent {
 	int xCenter;
 	int yCenter;
 	int r;
+	int t; 
+	int width; 
+	int height; 
+	int startAngle; 
+	int arcAngle; 
+	int cx;
+	int cy;
 	
 	protected void paintComponent(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
@@ -21,10 +28,10 @@ public class DrawingCanvas extends JComponent {
 		
 		xCenter = 400;
 		yCenter = 400;
-		r = 300;
+		r = 350;
 		
 		Ellipse2D.Double e = new Ellipse2D.Double(xCenter-r, yCenter-r, 2*r, 2*r);
-		g2d.setColor(Color.BLUE);
+		g2d.setColor(Color.YELLOW);
 		g2d.fill(e);
 		
 		xCenter = 400;
@@ -35,5 +42,88 @@ public class DrawingCanvas extends JComponent {
 		g2d.setColor(Color.GRAY);
 		g2d.fill(e2);
 		
+		t = 1; 
+		double rad = Math.toRadians(t); 
+		System.out.println("rad is " + rad); 
+		double sinValue = Math.sin(rad);
+		System.out.println("sin is " + sinValue);
+		double cosValue = Math.cos(rad); 
+		System.out.println("cos is " + cosValue); 
+		
+		double dWidth = r*(cosValue + (t*sinValue));
+		System.out.println("t*sinValue is " + t*sinValue);
+		System.out.println("double width is " + dWidth); 
+		
+		double dHeight = r*(sinValue - (t*cosValue));
+		width = (int)dWidth; 
+		height = -(int)dHeight; 
+		System.out.println("width is " + width);
+		System.out.println("height is " + height);
+		
+		arcAngle = (r/2) * t * t; 
+		
+		cx = 120;
+		cy = 110;
+		
+		g.setColor(Color.RED);
+		g.drawArc(cx,cy, width,height,0,arcAngle); 
+		
+		
+		
 	}
 }
+	/**	Path2D.Double p = new Path2D.Double();
+		p.moveTo(100,300);
+		p.lineTo(150,200);
+		p.lineTo(200,300);
+		p.closePath();
+		g2d.draw(p);
+		g2d.fill(p);
+	}*/
+	
+	/**private int width;
+	private int height;
+	private Cloud c1;
+	private Cloud c2;
+	private Cloud c3;
+	
+	public DrawingCanvas(int w, int h) {
+		width = w;
+		height = h;
+		c1 = new Cloud(10,50,75,Color.LIGHT_GRAY);
+		c2 = new Cloud(200,75,90,Color.BLUE);
+		c3 = new Cloud(420,60,85,Color.DARK_GRAY);
+	}
+	
+	protected void paintComponent(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		
+		RenderingHints rh = new RenderingHints(
+			RenderingHints.KEY_ANTIALIASING,
+			RenderingHints.VALUE_ANTIALIAS_ON);
+		g2d.setRenderingHints(rh);
+		
+		Rectangle2D.Double r = new Rectangle2D.Double(0,0,width,height);
+		g2d.setColor(new Color(100,149,237));
+		g2d.fill(r);
+		
+		Ellipse2D.Double e = new Ellipse2D.Double(200,75,100,100);
+		g2d.setColor(Color.BLUE);
+		g2d.fill(e);
+		
+		Line2D.Double line = new Line2D.Double(100,250,300,75);
+		g2d.setColor(Color.BLACK);
+		g2d.draw(line);
+		
+		Path2D.Double curve = new Path2D.Double();
+		curve.moveTo(250,400);
+		curve.curveTo(350,300,500,300,600,400);
+		g2d.draw(curve);
+		
+		c1.drawCloud(g2d);
+		c2.drawCloud(g2d);
+		c3.drawCloud(g2d);
+		
+	}*/
+
+
