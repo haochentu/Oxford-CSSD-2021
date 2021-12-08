83ae01e013bb17a2ffd11102f1922da8e9928234
Haochen Tu <67667094+haochentu@users.noreply.github.com>
2021-12-08 14:16:19 +0000

    use (x,y) coordinate to draw the involute curve
    it is the right way, has to figure out how to mirror the curve now

--- Involute_Gear/InvoluteTeeth.java
+++ Involute_Gear/InvoluteTeeth.java
@@ -2,50 +2,84 @@ import java.awt.*;
 import java.awt.geom.*;
 
 public class InvoluteTeeth {
-	private int cx;
-	private int cy;
 	private int r;
 	private int t; 
+	private double x1; 
+	private double y1;
+	private double x2;
+	private double y2;
+	private double xx;
+	private double yy; 
 	private int teethNumber; 
 	private Color color; 
 
-	public InvoluteTeeth(int cx, int cy, int r, int t, int teethNumber, Color color) {
-		this.cx = cx;
-		this.cy = cy;
+	public InvoluteTeeth(int r, int t, double x1, double y1, double x2, double y2, double xx, double yy, int teethNumber, Color color) {
+		//this.cx = cx;
+		//this.cy = cy;
 		this.r = r;
 		this.t = t;
+		this.x1 = x1;
+		this.y1 = y1;
+		this.x2 = x2;
+		this.y2 = y2; 
+		this.xx = xx;
+		this.yy = yy; 
 		this.teethNumber = teethNumber; 
 		this.color = color; 
 	}	
 	
-	public void drawInvoluteTeeth(Graphics2D g2d) {
+	protected void drawInvoluteTeeth(Graphics2D g2d) {
+		
 		g2d.setColor(color);
 		
-		double rad = Math.toRadians(t);  
-		double sinValue = Math.sin(rad);
-		double cosValue = Math.cos(rad); 
-		
-		double dWidth = r*(cosValue + (rad*sinValue));
-		double dHeight = r*(sinValue - (rad*cosValue));
-		int width = (int)dWidth; 
-		int height = (int)dHeight; 
-		
-		cy = cy - (height/4); 
-		
-		g2d.drawArc(cx ,cy, width, height, -19, -36); 
-		
-		for (int i = 0; i < teethNumber - 1; i++) {
-			g2d.rotate(Math.toRadians(360/teethNumber), 400, 400); 
-			g2d.drawArc(cx , cy, width, height, -19, -36);
+		for (int i = 0; i < 6; i++) {
+			
+			for (int i1 = 0; i1 < 50; i1++) {
+				double rad = Math.toRadians(t);  
+				double sinValue = Math.sin(t);
+				double cosValue = Math.cos(t); 
+				
+				if (x2 == 0) {
+					x1 = r*(cosValue + (rad*sinValue));
+					x1 = r + x1; 
+					xx = x1; 
+				} else {
+					x1 = x2; 
+				}
+				
+				if (y2 == 0) {
+					y1 = r*(sinValue - (rad*cosValue));
+					y1 = r + y1;
+					yy = y1; 
+				} else {
+					y1 = y2; 
+				}
+				
+				t += 1; 
+				
+				rad = Math.toRadians(t);  
+				sinValue = Math.sin(rad);
+				cosValue = Math.cos(rad); 
+				
+				x2 = r*(cosValue + (rad*sinValue));
+				x2 = r + x2; 
+				y2 = r*(sinValue - (rad*cosValue)); 
+				y2 = r - y2;
+				
+				if (t == 1) {
+					g2d.rotate(Math.toRadians(-90), (int)xx, (int)yy); 
+				}
+						
+				g2d.drawLine((int)x1,(int)y1,(int)x2,(int)y2);			
+				
 			}
-		
-		g2d.rotate(Math.toRadians(0), 400, 400); 
-		
-		g2d.drawArc(cx,cy, width, height, 19, 36); 
-		
-		for (int i = 0; i < teethNumber -1; i++) {
-			g2d.rotate(Math.toRadians(360/teethNumber), 400, 400); 
-			g2d.drawArc(cx ,cy, width, height, 19, 36);
-			}
+			
+			x2 = 0;
+			y2 = 0;
+			t = 0; 
+
+			g2d.rotate(Math.toRadians(90), (int)xx, (int)yy);
+			g2d.rotate(Math.toRadians(360/teethNumber), 400, 400);
+		}
 	}
 }
