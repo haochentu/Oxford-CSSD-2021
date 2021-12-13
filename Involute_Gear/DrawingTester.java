e4c2400b8055c9aa2766cd02faae43e159e94f89
Haochen Tu <67667094+haochentu@users.noreply.github.com>
2021-12-13 19:06:57 +0000

    Adding teethNumber and circleSize parameters

--- Involute_Gear/DrawingTester.java
+++ Involute_Gear/DrawingTester.java
@@ -5,12 +5,14 @@ public class DrawingTester {
 	public static void main(String[] args) {
 		int w = 800;
 		int h = 800;
+		int r = 200; 
+		int teethNumber = 12; 
+		int circleDiameter = 400;
 		JFrame f = new JFrame();
-		//DrawingCanvas dc = new DrawingCanvas(w,h);
-		DrawingCanvas dc = new DrawingCanvas();
+		DrawingCanvas dc = new DrawingCanvas(w, h, r, teethNumber, circleDiameter);
 		
 		f.setSize(w,h);
-		f.setTitle("Drawing in Java");
+		f.setTitle("Involute Gear");
 		f.add(dc);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setVisible(true);
